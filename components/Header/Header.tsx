eb220a671774b5a92cf503bd4e4ca876cdda70b0
Oleksandra <askibina12@gmail.com>
2026-04-13 00:11:00 +0200

    Done Home page

---
+++ components/Header/Header.tsx
@@ -0,0 +1,79 @@
+import Link from "next/link";
+import css from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={css.header}>
+      <ul className={css.social}>
+        <li className={css.socialLink}>
+          <a
+            href="https://github.com/SashaSkybina11"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg className={css.icon} width="30" height="30">
+              <use href="/icons/symbol-defs.svg#icon-github" />
+            </svg>
+          </a>
+        </li>
+        <li className={css.socialLink}>
+          <a href="mailto:sashaskibina16@gmail.com" aria-label="Send email">
+            <svg className={css.icon} width="30" height="30">
+              <use href="/icons/symbol-defs.svg#icon-email" />
+            </svg>
+          </a>
+        </li>
+        <li className={css.socialLink}>
+          <a
+            href="https://www.linkedin.com/in/oleksandraskybina/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg className={css.icon} width="30" height="30">
+              <use href="/icons/symbol-defs.svg#icon-linkedin" />
+            </svg>
+          </a>
+        </li>
+      </ul>
+      <Link href={""} className={css.logo}>
+        <p className={css.logoDescr}>
+          <span>O</span>
+          <span className={css.dot}>.</span>
+          <span>S</span>
+          <span>k</span>
+          <span>y</span>
+          <span>b</span>
+          <span>i</span>
+          <span>n</span>
+          <span>a</span>
+        </p>
+      </Link>
+      <nav>
+        <ul className={css.nav}>
+          <li className={css.item}>
+            <Link className={css.itemLink} href="">
+              home
+            </Link>
+          </li>
+          <li className={css.item}>
+            <Link className={css.itemLink} href="">
+              contact
+            </Link>
+          </li>
+          <li className={css.item}>
+            <Link className={css.itemLink} href="">
+              projects
+            </Link>
+          </li>
+          <li className={css.item}>
+            <Link className={css.itemLink} href="">
+              about me
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ types/skills.tsx
@@ -0,0 +1,11 @@
+export const skills = [
+  "DEVELOPER",
+  "FRONTEND",
+  "HTML",
+  "CSS",
+  "JAVASCRIPT",
+  "REACT",
+  "NEXT",
+  "TYPESCRIPT",
+  "GITHUB",
+];
