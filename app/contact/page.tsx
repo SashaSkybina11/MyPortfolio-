f2215d01a7d71af01740c5c565f8b92c03de9d27
Oleksandra <askibina12@gmail.com>
2026-04-23 16:18:33 +0200

    Add icons-contact, the contact page with adaptive functionality is complete

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,21 +1,31 @@
 import { skills } from "@/types/skills";
 import css from "./Page.module.css";
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import { TbBrandLinkedin } from "react-icons/tb";
+import { VscGithub } from "react-icons/vsc";
 
 const Page = () => {
   return (
-    <main className={css.containerContact}>
+    <main className={css.mainContact}>
       <div className={css.contentContact}>
         <h1 className={css.title}>Contact</h1>
         <div className={css.descr}>
           <div className={css.descrItem}>
-            <p className={css.descrTitle}>Email:</p>
+            <div className={css.descrBox}>
+              <MdOutlineAlternateEmail className={css.descrIcon} />
+              <p className={css.descrTitle}>Email:</p>
+            </div>
             <a href="mailto:sashaskibina16@gmail.com" className={css.descrLink}>
               sashaskibina16@gmail.com
             </a>
           </div>
 
           <div className={css.descrItem}>
-            <p className={css.descrTitle}>LinkedIn:</p>
+            <div className={css.descrBox}>
+              <TbBrandLinkedin className={css.descrIcon} />
+              <p className={css.descrTitle}>LinkedIn:</p>
+            </div>
+
             <a
               href="https://www.linkedin.com/in/oleksandraskybina/"
               className={css.descrLink}
@@ -27,7 +37,10 @@ const Page = () => {
           </div>
 
           <div className={css.descrItem}>
-            <p className={css.descrTitle}>GitHub:</p>
+            <div className={css.descrBox}>
+              <VscGithub className={css.descrIcon} />
+              <p className={css.descrTitle}>GitHub:</p>
+            </div>
             <a
               href="https://github.com/SashaSkybina11"
               className={css.descrLink}
@@ -38,28 +51,29 @@ const Page = () => {
             </a>
           </div>
         </div>
-        {/* <div className={css.whiteLine}>
-          <div className={css.track}>
-            {[...skills, ...skills].map((skill, i) => (
-              <div key={i} className={css.item}>
-                <span className={css.whiteText}>{skill}</span>
-                <span className={css.dot}></span>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className={css.grayLine}>
-          <div className={css.track}>
-            {[...skills, ...skills].reverse().map((skill, i) => (
-              <div key={i} className={css.item}>
-                <span className={css.grayText}>{skill}</span>
-                <span className={css.dot}></span>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className={css.behind}>Contact</div> */}
       </div>
+      <div className={css.whiteLine}>
+        <div className={css.track}>
+          {[...skills, ...skills].map((skill, i) => (
+            <div key={i} className={css.item}>
+              <span className={css.whiteText}>{skill}</span>
+              <span className={css.dot}></span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={css.grayLine}>
+        <div className={css.track}>
+          {[...skills, ...skills].reverse().map((skill, i) => (
+            <div key={i} className={css.item}>
+              <span className={css.grayText}>{skill}</span>
+              <span className={css.dot}></span>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className={css.behind}>Contact</div>
     </main>
   );
 };
